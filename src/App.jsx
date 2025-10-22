dfacadad887d1f5ff97e3c5063063028fdc17f9f
Sam Meade <sammeade1990@outlook.com>
2025-10-22 14:14:01 +0100

    Fixed file structure issue for SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, {useState } from "react";
 import "./App.css";
-import SearchBar from ".components/SearchBar/SearchBar";
+import SearchBar from "./components/SearchBar/SearchBar";
 import SearchResults from "./components/SearchResults/SearchResults";
 import Playlist from "./components/Playlist/Playlist";
 
