6a917a0912c698591a43c5c0d10f5b4e2eb0eb0a
Sam Meade <sammeade1990@outlook.com>
2025-10-24 13:58:23 +0100

    Added functionality so users can change the name of their playlist

--- src/components/Playlist/Playlist.jsx
+++ src/components/Playlist/Playlist.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import TrackList from "../TrackList/TrackList";
 
-function Playlist({ playlistName, tracks, onRemove }) {
+function Playlist({ playlistName, tracks, onRemove, onNameChange }) {
     return (
         <div className="playlist">
-            <input type="text" value={playlistName} onChange={(e) => onNameChange(e.target.value)} />
+            <input type="text" value={playlistName} onChange={(e) => onNameChange(e.target.value)} placeholder="Enter your playlist name" />
             <TrackList tracks={tracks} onRemove={onRemove} />
         </div>
     );
